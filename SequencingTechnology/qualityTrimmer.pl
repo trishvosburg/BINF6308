0631dbab6c1dfa76352f8245a76d6807d187c0b7
vosburg-p <vosburg.p@husky.neu.edu>
2018-01-22 19:49:47 -0500

    2nd draft of qualityTrimmer.pl, can't figure out how to interleave

--- SequencingTechnology/qualityTrimmer.pl
+++ SequencingTechnology/qualityTrimmer.pl
@@ -11,16 +11,21 @@ use Bio::SeqIO;
 my $seqio_obj_R1 = Bio::SeqIO->new(-file => 'Sample.R1.fastq',
 							-format => 'fastq');		
 # Create $left 
-while (my $seq_obj_R1 = $seqio_obj_R1->next_seq){
-	my $left=$seq_obj_R1;
+my $left = $seqio_obj_R1->next_seq;
+#while (my $seq_obj_R1 = $seqio_obj_R1->next_seq){
+	#my $left=$seq_obj_R1;
+
+################################
 
 #One for Sample.R2.fastq
 my $seqio_obj_R2 = Bio::SeqIO->new(-file => 'Sample.R2.fastq',
 							-format => 'fastq');
 # Create $right
-while (my $seq_obj_R2 = $seqio_obj_R2->next_seq){
-	my $right=$seq_obj_R2;
-
+my $right = $seqio_obj_R2->next_seq;
+#while (my $seq_obj_R2 = $seqio_obj_R2->next_seq){
+	#my $right=$seq_obj_R2;
+	
+#################################
 #Bio:SeqIO for a fastq file returns a Bio::Seq:Quality, method:
 #Returns the longest sequence that has quality values above the given threshold
 my $leftTrimmed = $left->get_clear_range(20);
