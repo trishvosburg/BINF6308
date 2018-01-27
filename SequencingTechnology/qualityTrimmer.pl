932abf92f7ef906919e0d842c12998b0c7115490
vosburg-p <vosburg.p@husky.neu.edu>
2018-01-26 22:52:24 -0500

    3rd draft qualityTrimmer, need to figure out how to write output file

--- SequencingTechnology/qualityTrimmer.pl
+++ SequencingTechnology/qualityTrimmer.pl
@@ -6,41 +6,45 @@ use diagnostics;
 use Bio::Seq;
 use Bio::SeqIO;
 
-#need three Bio::SeqIO objects
-#One for Sample.R1.fastq
-my $seqio_obj_R1 = Bio::SeqIO->new(-file => 'Sample.R1.fastq',
-							-format => 'fastq');		
-# Create $left 
-my $left = $seqio_obj_R1->next_seq;
-#while (my $seq_obj_R1 = $seqio_obj_R1->next_seq){
-	#my $left=$seq_obj_R1;
-
-################################
-
-#One for Sample.R2.fastq
-my $seqio_obj_R2 = Bio::SeqIO->new(-file => 'Sample.R2.fastq',
-							-format => 'fastq');
-# Create $right
-my $right = $seqio_obj_R2->next_seq;
-#while (my $seq_obj_R2 = $seqio_obj_R2->next_seq){
-	#my $right=$seq_obj_R2;
-	
-#################################
-#Bio:SeqIO for a fastq file returns a Bio::Seq:Quality, method:
-#Returns the longest sequence that has quality values above the given threshold
-my $leftTrimmed = $left->get_clear_range(20);
-my $rightTrimmed = $right->get_clear_range(20);
-
-#to copy description from one Bio::Seq to another:
-$leftTrimmed->desc($left->desc());
-$rightTrimmed->desc($right->desc());
+#Need three Bio::SeqIO objects
+#One for each fastq
+my $seqio_obj_L = Bio::SeqIO->new(
+	-file   => 'Sample.R1.fastq',
+	-format => 'fastq'
+);
+my $seqio_obj_R = Bio::SeqIO->new(
+	-file   => 'Sample.R2.fastq',
+	-format => 'fastq'
+);
 
 #Create Bio::SeqIO for Interleaved.fastq (output)
-my $seq_obj_out = Bio::Seq->new(
 
-);
 my $seqio_obj_out = Bio::SeqIO->new(
-			-file => '>Interleaved.fastq',
-			-format => 'fastq'
+	-file   => '>Interleaved.fastq',
+	-format => 'fastq'
 );
+
+#Create $left, inside while loop create right read SeqIO
+while ( my $seq_obj_L = $seqio_obj_L->next_seq ) {
+	while ( my $seq_obj_R = $seqio_obj_R->next_seq ) {
+		my $left  = $seq_obj_L;
+		my $right = $seq_obj_R;
+
+ 	#Bio:SeqIO for a fastq file returns a Bio::Seq:Quality, method:
+ 	#Returns the longest sequence that has quality values above the given threshold
+		my $leftTrimmed  = $left->get_clear_range(20);
+		my $rightTrimmed = $right->get_clear_range(20);
+
+	#to copy description from one Bio::Seq to another:
+		$leftTrimmed->desc( $left->desc() );
+		$rightTrimmed->desc( $right->desc() );
+		
+	#Write output file
+		my $seq_obj_out = Bio::Seq->new(
+			-seq => "",
+			-display_id => "",
+			-desc => "",
+		);
+	}
+}
 
