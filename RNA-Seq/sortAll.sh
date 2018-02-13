35fa8ab4aa13f073e694e0441240678469cd099f
vosburg-p <vosburg.p@husky.neu.edu>
2018-02-13 18:17:26 -0500

    2nd draft sortAll.sh

--- RNA-Seq/sortAll.sh
+++ RNA-Seq/sortAll.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #sortAll should output to bam directory
-pairedTrimmedPath="/Paired/"
+pairedTrimmedPath="Paired/"
 leftSuffix=".R1.fastq"
 rightSuffix=".R2.fastq"
-outPath="sam/"
+outPath="bam/"
 for leftInFile in $pairedTrimmedPath*$leftSuffix
 do
 	pathRemoved="${leftInFile/$pairedTrimmedPath/}"
 	sampleName="${pathRemoved/$leftSuffix/}"
 	echo samtools sort \
 	Aip02.sam \
-	-o Aip02.sorted.bam
-	$outPath$sampleName$leftSuffix \
-	$outPath$sampleName$rightSuffix \
-	#1>$sampleName.sort.log 2>$sampleName.sort.err 
+	#1>$outPath$sampleName \
+	#$outPath.sorted.bam \
+	#$sampleName.sort.log 2>$sampleName.sort.err 
 
 done
