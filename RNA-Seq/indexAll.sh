7830ea9c4a3da9bf16c22f5d086582c2e5894b78
vosburg-p <vosburg.p@husky.neu.edu>
2018-02-13 18:17:38 -0500

    2nd draft indexAll.sh

--- RNA-Seq/indexAll.sh
+++ RNA-Seq/indexAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #indexAll should output to bam directory
-pairedTrimmedPath="/Paired"
+pairedTrimmedPath="Paired/"
 leftSuffix=".R1.fastq"
 rightSuffix=".R2.fastq"
 outPath= "bam/"
@@ -10,8 +10,7 @@ do
 	sampleName="${pathRemoved/$leftSuffix/}"
 	echo nice -n19 \
 	samtools index Aip02.sorted.bam \
-	-o Aip02.index.bam \
-	$outPath$sampleName$leftSuffix \
-	$outPath$sampleName$rightSuffix \
+	#1>$outPath$sampleName \
+	$sampleName.index.bam \
 	1>$sampleName.index.log 2>$sampleName.index.err 
 done
