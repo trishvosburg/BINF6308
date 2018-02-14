452eee2db18ed4f8b1fe03fa0e7cbc1817bf95b9
vosburg-p <vosburg.p@husky.neu.edu>
2018-02-14 16:30:25 -0500

    final submission indexAll.sh

--- RNA-Seq/indexAll.sh
+++ RNA-Seq/indexAll.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-#indexAll should output to bam directory
-bamPath="bam/"
-suffix=".sorted.bam"
+bamPath='bam/'
+suffix='.aligned.sorted.bam'
 outPath="bam/"
-for LeftInFile in $bamPath*$suffix
+for leftInFile in $bamPath*$suffix
 do
 	pathRemoved="${leftInFile/$bamPath/}"
 	sampleName="${pathRemoved/$suffix/}"
 	samtools index \
 	$bamPath$sampleName$suffix \
-	-o $outPath$sampleName.index.bai \
 	1>$sampleName.index.log 2>$sampleName.index.err 
 done
