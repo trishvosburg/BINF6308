bfe17c3a31e317769989fa9ead05cb7a7c376352
vosburg-p <vosburg.p@husky.neu.edu>
2018-02-25 14:24:24 -0500

    3rd draft mergeAnnotationsGo, issue with no ouput

--- RNA-Seq/mergeAnnotationsGo.pl
+++ RNA-Seq/mergeAnnotationsGo.pl
@@ -4,10 +4,33 @@ use strict;
 
 # Purpose: merge the annotations from tsv file by putting data in 2D hash
 
-open( SP, "<", "aipSwissProt.tsv" ) or die "Cannot open file", $!;
+# Add read filehandle to so it reads bioProcess.tsv from navigateGoTerms.pl
+# Puts IDs and N=names as hash -> ID as key, name as value
+# Modify output so it prints trinityID, SwissProtID, SwissProt description, GO Terms, and GO descriptions
+# for biological process terms associated with the trinity IDs
+# write to trinitySPGo.tsv
 
+# Open tsv file from Blast2Go with filehandle, or die
+open( SP_TO_GO, "<", "spToGo.tsv" ) or die $!;
+
+# Declare hash
 my %spToGo;
 
+# Loop through tsv file and chomp to get rid of any excess characters
+while (<SP_TO_GO>) {
+	chomp;
+
+# Split each line in $_ from the tsv file with tabs, assign to $swissprot and $go
+	my ( $swissProt, $go ) = split( "\t", $_ );
+
+	# Store in hash as keys. Increment counter as value
+	$spToGo{$swissProt}{$go}++;
+}
+
+# Open tsv file with filehandle, or die
+open( SP, "<", "aipSwissProt.tsv" ) or die $!;
+
+# Loop through tsv file and chomp to get rid of any excess characters
 while (<SP>) {
 	chomp;
 	my ( $trinity, $swissProt, $description, $eValue ) =
@@ -20,24 +43,6 @@ while (<SP>) {
 }
 
 =cut
-# Open tsv file from Blast2Go with filehandle, or die
-open( SP_TO_GO, "<", "spToGo.tsv" ) or die $!;
-
-# Declare hash
-my %spToGo;
-
-# Loop through tsv file and chomp to get rid of any excess characters
-while (<SP_TO_GO>) {
-	chomp;
-
-	# Split each line in $_ from the tsv file with tabs,
-	# assign to $swissprot and $go
-	my ( $swissProt, $go ) = split( "\t", $_ );
-
-	# Store in hash as keys. Increment counter as value
-	$spToGo{$swissProt}{$go}++;
-}
-
 # Test hash with foreach loop
 # Loop over $swissProt (first key) and sort to make sure everything is in order
 foreach my $swissProt ( sort keys %spToGo ) {
