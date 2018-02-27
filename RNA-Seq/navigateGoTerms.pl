1c517ab78e25f3a8728fa958bd10b41b320e6fed
vosburg-p <vosburg.p@husky.neu.edu>
2018-02-26 21:44:45 -0500

    Final draft navigateGoTerms, formatted correctly

--- RNA-Seq/navigateGoTerms.pl
+++ RNA-Seq/navigateGoTerms.pl
@@ -2,8 +2,8 @@
 use warnings;
 use strict;
 
-# Purpose: to understand BioPerl OntologyIO module, which can read 
-	#gene onotology OBO format
+# Purpose: to understand BioPerl OntologyIO module, which can read
+#gene onotology OBO format
 
 # Add write filehandle so tab-separated output is written to bioProcess.tsv
 
@@ -14,12 +14,33 @@ use Bio::OntologyIO;
 # Constructs whole hierarchy of GO terms
 my $parser = Bio::OntologyIO->new(
 	-format => "obo",
-	-file => "go-basic.obo"
+	-file   => "go-basic.obo"
 );
 
+# Create output file
+my $outfile = 'bioProcess.tsv';
+my $outFh;
+
+# If you can't open the outfile, die
+unless ( open( $outFh, ">", $outfile ) ) {
+	die join( ' ', "Can't open outfile", $outfile, $! );
+}
+
 # Keeps returning top level terms until it reaches end of file
-while (my $ont = $parser->next_ontology() ) {
-	print "read ontology ", $ont->name(), "with ",
-		scalar ($ont->get_root_terms), " root terms, and ",
-		scalar ($ont->get_leaf_terms), " leaf terms \n";
+# Use the parser to find the biological process terms only
+while ( my $ont = $parser->next_ontology() ) {
+
+	# If the GO term is biological_process,
+	if ( $ont->name() eq "biological_process" ) {
+
+		# for each leaf, get the GO name and GO identifier
+		foreach my $leaf ( $ont->get_leaf_terms ) {
+			my $go_name = $leaf->name();
+			my $go_id   = $leaf->identifier();
+
+			# add to outfile
+			print $outFh join( "\t", $go_id, $go_name ), "\n";
+		}
+	}
+
 }
