5ec99701a900acb2f0f1c3dabc89c25a8da7132a
vosburg-p <vosburg.p@husky.neu.edu>
2018-04-14 19:36:50 -0400

    VariantCalling final

--- VariantCalling/findVariants.sh
+++ VariantCalling/findVariants.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Make vcf directory
+# For all the bam files in the noDup folder,
+# Make paths for outfiles
+# Use genome analysis toolkit's Haplotype Caller as the variant discovery tool
+# emitRefConfidence dictates that the reference model is emitted with GVCF format
+# Reference is vShiloni.fasta, -I is the bam input
+# genotyping_mode DISCOVERY specifies how to determine the alternate alleles for genotyping
+# variant_index_type specifies the type of index creator to use for VCF indices (LINEAR)
+# variant_index_parameter dictates the parameter # to pass on to the VCF IndexCreator
+# stand_call_conf is the minimum phred-scaled confidence threshold at which variants should be called (30)
+# nct is the multi-threaded mode (16 in this case), the # of data threds to allocate to this analysis
 mkdir -p vcf
 findVariants(){
 for bam in noDup/*.bam
--- VariantCalling/recalibrateBases.sh
+++ VariantCalling/recalibrateBases.sh
@@ -2,20 +2,28 @@
 # VCF file as input for known variants
 # noDup bam files and filteredSnps.vcf as input
 # Generate before/after plots
-knownVariant=''
+# Use 3 different subroutines? 
+knownVariant='filteredSnps.vcf'
 noDupPath='noDup/'
 recalData='recal_data.table'
 postRecalData='post_recal_data.table'
 pdfOut='recalibration_plots.pdf'
 bamOut='recal_reads.bam'
-##
+# stepone(){
+#	for bamFile in noDup/*.bam
+#	do
+#		nice -n19 java -jar usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar 
+#		-T BaseRecalibrator -R genotype.vcf -I $noDupPath -L 20 -knownSites $knownVariant -o $recalData
+#	done
+#}
+#stepOne 1>step1.log 2>step1.err &
+
 baseCommand='nice -n19 java -jar usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
-step1='-T BaseRecalibrator -R genotype.vcf' 
-step2='-T BaseRecalibrator -R genotype.vcf'
+step12='-T BaseRecalibrator -R genotype.vcf' 
 step3='-T AnalyzeCovariates -R genotype.vcf'
-step4='-T PrintReads  -R genotype.vcf'
-$baseCommand $step1 -I $noDupPath -L 20 -knownSites $knownVariant -o $recalData \
-$baseCommand $step2 -I $noDupPath -L 20 -knownSites $knownVariant -BSQR $recalData -o $postRecalData \
+step4='-T PrintReads -R genotype.vcf'
+$baseCommand $step12 -I $noDupPath -L 20 -knownSites $knownVariant -o $recalData \
+$baseCommand $step12 -I $noDupPath -L 20 -knownSites $knownVariant -BSQR $recalData -o $postRecalData \
 $baseCommand $step3 -L 20 -before $recalData -after $postRecalData -plots $pdfOut
 $baseCommand $step4 -I $noDupPath -L 20 -BSQR $recalData -o $bamOut \
 1>recalibrate.log 2>recalibrate.err &
