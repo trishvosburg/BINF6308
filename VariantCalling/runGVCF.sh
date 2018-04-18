310126addb27201eb0301b36593e9d9619deb597
vosburg-p <vosburg.p@husky.neu.edu>
2018-04-17 21:18:00 -0400

    runGVCF

--- VariantCalling/runGVCF.sh
+++ VariantCalling/runGVCF.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 vcf="$(ls -q vcf/*.vcf)"
 #vcf="$(echo $vcf)"
-inPath='vcf/'
 replacement='--variant vcf/'
 vcfParam="${vcf//$inPath/$replacement}"
 baseCommand='nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
